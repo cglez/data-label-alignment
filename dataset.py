b27131371d4e796e89ecb3f6591be3201c68b7f7
César González <ceguel@gmail.com>
2025-05-09 19:43:30 +0200

    Improve dataset to_json

--- dataset.py
+++ dataset.py
@@ -6,20 +6,23 @@ import fire
 from datasets import load_dataset, Value
 
 
-def download(dataset, split='train', cast=False):
+def to_json(dataset, split='train', label='label'):
     data_files = {os.path.splitext(os.path.basename(x))[0]: x for x in glob(f'data/datasets/{dataset}/*.csv')}
     ds = load_dataset('csv', data_files=data_files, split=split)
-    ds = ds.rename_column('Unnamed: 0', 'id')
+
+    if 'id' not in ds.column_names:
+        ds = ds.rename_column('Unnamed: 0', 'id')
+    if label != 'label':
+        ds = ds.rename_column(label, 'label')
     ds = ds.rename_column('text', 'data')
+    ds = ds.select_columns(['id', 'data', 'label'])
 
-    if cast:
-        ds = ds.cast_column("label", Value("string"))
-        classes = ds.features["label"]
-        ds = ds.map(lambda x: {"label": classes.int2str(x['label'])})
+    if ds.features['label'] == Value('int64'):
+        ds = ds.cast_column('label', Value('string'))
 
-    with open(f"data/{dataset}.json", "w", encoding="utf-8") as f:
+    with open(f'data/{dataset}.json', 'w', encoding='utf-8') as f:
         json.dump(ds.to_list(), f, indent=0, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    fire.Fire(download)
+    fire.Fire(to_json)
